90c9526f29459c5e9734e0bf998b2e4593b7ae55
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-08-27 21:54:08 +0100

    Add statically CDN

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const prod = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  assetPrefix: prod ? 'https://cdn.statically.io/gh/ainsleyrutterford/ainsleyrutterford.github.io/nextjs/gh-pages/' : '',
   distDir: 'build',
   reactStrictMode: true,
 }
