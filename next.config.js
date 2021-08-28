f24461746b795eb34eff51d5bc4f01e0a64064dd
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-08-28 18:48:03 +0100

    Clean up .gitignore and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-/** @type {import('next').NextConfig} */
-
 // https://github.com/vercel/next.js/issues/18356#issuecomment-720449235
 const withOptimizedImages = require("next-optimized-images");
 
 const prod = process.env.NODE_ENV === "production";
-const repoName = process.env.NEXT_PUBLIC_REPO_NAME;
 
 module.exports = withOptimizedImages({
-  assetPrefix: prod ? `/${repoName}/` : ".",
+  assetPrefix: prod ? `/${process.env.NEXT_PUBLIC_REPO_NAME}/` : ".",
   reactStrictMode: true,
   // https://github.com/vercel/next.js/issues/21079
   images: {
