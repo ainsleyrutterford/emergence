477725181ab8867656491684951568a4c6e48f85
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-08-28 18:11:37 +0100

    Remove env-config.js

--- env-config.js
+++
@@ -1,6 +0,0 @@
-// const prod = process.env.NODE_ENV === "production";
-// const repoName = process.env.NEXT_PUBLIC_REPO_NAME;
-
-// module.exports = {
-//   "process.env.BACKEND_URL": prod ? `/${repoName}` : "",
-// };
