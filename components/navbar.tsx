a0d2ed90cf8527bb50e85ee81b5a31e8e8f4c971
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-09-16 20:48:36 -0400

    Style Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,10 +6,10 @@ export const Navbar = () => {
   const { style, setStyle } = useContext(StyleContext);
   return (
     <>
-      <div style={{ backgroundColor: "white", position: "fixed", minWidth: "100%", minHeight: "50px" }}>
-        <button onClick={() => setStyle({ ...style, style: Style.LaTeX })}>LaTeX style</button>
-        <button onClick={() => setStyle({ ...style, style: Style.GitHub })}>GitHub style</button>
-        <button onClick={() => setStyle({ ...style, darkmode: !style.darkmode })}>Toggle darkmode</button>
+      <div className={style.darkmode ? "navbar-dark" : "navbar-light"} style={{ position: "fixed", minWidth: "100%", minHeight: "50px" }}>
+        <button onClick={() => setStyle({ ...style, style: Style.LaTeX })}>LaTeX</button>
+        <button onClick={() => setStyle({ ...style, style: Style.GitHub })}>GitHub</button>
+        <button onClick={() => setStyle({ ...style, darkmode: !style.darkmode })}>Darkmode</button>
       </div>
       <div style={{ minHeight: "50px" }} />
     </>
