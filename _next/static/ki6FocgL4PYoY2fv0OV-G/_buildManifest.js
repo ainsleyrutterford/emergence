ed3f334d5014d54ba65be54ff627f898ee83a2d8
ainsleyrutterford <ar16478@bristol.ac.uk>
2021-09-18 17:57:51 +0000

    Deploying to gh-pages from @ ainsleyrutterford/emergence@7aed96513d8bdeb0bc29dc6718c80443cb6d8e5f 🚀

---
+++ _next/static/ki6FocgL4PYoY2fv0OV-G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0cadeaeacb1533f7e8ba.css","static/chunks/pages/index-f43e4ace44c1dd845e8d.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
