8aa67280303153ad11b93e044044377792775ae7
ainsleyrutterford <ar16478@bristol.ac.uk>
2021-09-23 23:57:44 +0000

    Deploying to gh-pages from @ ainsleyrutterford/emergence@7f63266b6d6b2f66ff7b72096a9e59e48bee0d53 🚀

--- theme.js
+++ theme.js
@@ -5,16 +5,19 @@ if (!document.documentElement.hasAttribute("color-mode")) {
   const getInitialColorMode = () => {
     const localStorageColorMode = window.localStorage.getItem("color-mode");
     const hasStoragePreference = typeof localStorageColorMode === "string";
+
     // If the user has explicitly chosen light or dark, use it
     if (hasStoragePreference) {
       return localStorageColorMode;
     }
+
     // If they haven't been explicit, check the system preference
     const query = window.matchMedia("(prefers-color-scheme: dark)");
     const hasSystemPreference = typeof query.matches === "boolean";
     if (hasSystemPreference) {
       return query.matches ? "dark" : "light";
     }
+
     // Default to "light".
     return "light";
   };
