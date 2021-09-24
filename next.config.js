e75f66f74792d3f6b36b3819ec51ecfb30cf8695
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-09-23 23:41:31 -0400

    Revert en-GB locale

--- next.config.js
+++ next.config.js
@@ -3,8 +3,4 @@ const prod = process.env.NODE_ENV === "production";
 module.exports = {
   assetPrefix: prod ? `/${process.env.NEXT_PUBLIC_REPO_NAME}/` : ".",
   reactStrictMode: true,
-  i18n: {
-    locales: ["en-GB"],
-    defaultLocale: "en-GB",
-  },
 };
