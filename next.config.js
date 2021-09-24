1898a0c290340e50cfb0fcb2f7953cf537575bcf
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-09-23 23:37:24 -0400

    Add en-GB locale

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const prod = process.env.NODE_ENV === "production";
 module.exports = {
   assetPrefix: prod ? `/${process.env.NEXT_PUBLIC_REPO_NAME}/` : ".",
   reactStrictMode: true,
+  i18n: {
+    locales: ["en-GB"],
+    defaultLocale: "en-GB",
+  },
 };
