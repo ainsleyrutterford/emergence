9a8c871f4e11c73d28b07b47d7c06ce228a00578
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2021-09-27 23:13:52 +0100

    Add comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,11 +17,14 @@ class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head>
+          {/* This is the default <head> for all pages across the website. */}
           <script id="theme" src="theme.js" />
           <link
             rel="icon"
             href={`/${process.env.NEXT_PUBLIC_REPO_NAME}/favicon.ico`}
           />
+          {/* This meta should be put in the Head for each page as it will not be the
+              same, whereas the two above are the same for all pages */}
           <meta
             name="description"
             content="Emergence hosts a series of articles exploring interesting algorithms, phenomena, and topics from various fields."
