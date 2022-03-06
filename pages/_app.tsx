2e2c8b300c251449c0c31e3da90f912eba341620
Ainsley Rutterford <ainsley.rutterford@bardsley-england.com>
2022-03-06 00:07:55 +0000

    Store article style in local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,27 +4,28 @@ import "../styles/global.css";
 import type { AppProps } from "next/app";
 import React, { createContext, useEffect, useState } from "react";
 
-export enum Style {
-  LaTeX,
-  GitHub,
-}
+const defaultContext = {
+  colorMode: "",
+  setColorMode: (mode: string) => {},
+  articleStyle: "",
+  setArticleStyle: (style: string) => {},
+};
 
-export const ThemeContext = createContext<{
-  colorMode: string;
-  setColorMode: (mode: string) => void;
-  articleStyle: Style;
-  setArticleStyle: (style: Style) => void;
-}>({} as any);
+export const ThemeContext = createContext(defaultContext);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [colorMode, setColorMode] = useState("");
-  const [articleStyle, setArticleStyle] = useState(Style.LaTeX);
+  const [articleStyle, setArticleStyle] = useState("");
 
   // Runs once on initial render. Gets the color mode from the HTML element.
   useEffect(() => {
     const initialColorMode =
       document.documentElement.getAttribute("color-mode");
+    const initialArticleStyle =
+      document.documentElement.getAttribute("article-style");
+
     setColorMode(initialColorMode === "dark" ? "dark" : "light");
+    setArticleStyle(initialArticleStyle === "github" ? "github" : "latex");
   }, []);
 
   // Runs each time colorMode is updated. Update the HTML element and store
@@ -35,6 +36,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       window.localStorage.setItem("color-mode", colorMode);
     }
   }, [colorMode]);
+
+  // Runs each time articleStyle is updated. Store the new value in local storage
+  useEffect(() => {
+    if (articleStyle !== "") {
+      window.localStorage.setItem("article-style", articleStyle);
+    }
+  }, [articleStyle]);
 
   return (
     <ThemeContext.Provider
