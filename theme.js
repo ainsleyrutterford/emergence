b6d8a215cd47ba215febe99c2b2f15ee7cb7fd11
ainsleyrutterford <ainsley.rutterford@gmail.com>
2022-04-10 23:19:48 +0000

    Deploying to gh-pages from @ ainsleyrutterford/emergence@478902ab66be3bd029b974dd6d4a0b4ac71d9878 🚀

--- theme.js
+++ theme.js
@@ -7,9 +7,7 @@ if (!document.documentElement.hasAttribute("color-mode")) {
     const hasStoragePreference = typeof localStorageColorMode === "string";
 
     // If the user has explicitly chosen light or dark, use it
-    if (hasStoragePreference) {
-      return localStorageColorMode;
-    }
+    if (hasStoragePreference) return localStorageColorMode;
 
     // If they haven't been explicit, check the system preference
     const query = window.matchMedia("(prefers-color-scheme: dark)");
@@ -22,6 +20,21 @@ if (!document.documentElement.hasAttribute("color-mode")) {
     return "light";
   };
 
+  const getInitialArticleStyle = () => {
+    const localStorageArticleStyle =
+      window.localStorage.getItem("article-style");
+    const hasStoragePreference = typeof localStorageArticleStyle === "string";
+
+    // If the user has explicitly chosen an article style, use it
+    if (hasStoragePreference) return localStorageArticleStyle;
+
+    // Default to "latex".
+    return "latex";
+  };
+
   const colorMode = getInitialColorMode();
+  const articleStyle = getInitialArticleStyle();
+
   document.documentElement.setAttribute("color-mode", colorMode);
+  document.documentElement.setAttribute("article-style", articleStyle);
 }
